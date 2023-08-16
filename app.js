f7b48579ce5a8cde39253a461a721406e8d67b07
Remaker00 <93196599+Remaker00@users.noreply.github.com>
2023-08-16 13:17:40 +0530

    version1.10

---
+++ app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const _404error = require('./controllers/error');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(_404error.error);
+
+app.listen(3000);
